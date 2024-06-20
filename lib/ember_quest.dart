3dc3fc8e54614801d821e6ebce23d92b4aa7ebc0
kshLithium <2071123@hansung.ac.kr>
2024-06-20 11:22:06 +0900

    Loading Assets

---
+++ lib/ember_quest.dart
@@ -0,0 +1,17 @@
+import 'package:flame/game.dart';
+
+class EmberQuestGame extends FlameGame {
+  EmberQuestGame();
+
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png'
+    ]);
+  }
+}
