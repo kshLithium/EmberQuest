aec8068904d05d61de110a0b2f9f118c916e7924
kshLithium <2071123@hansung.ac.kr>
2024-06-20 23:55:01 +0900

    build segments & refactoring InitGame in ember_quest.dart

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -1,9 +1,41 @@
 import 'package:ember_quest/actors/ember.dart';
+import 'package:ember_quest/objects/ground_block.dart';
+import 'package:ember_quest/objects/platform_block.dart';
+import 'package:ember_quest/objects/star.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'managers/segment_manager.dart';
+import 'actors/water_enemy.dart';
 
 class EmberQuestGame extends FlameGame {
   late EmberPlayer _ember;
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+        case PlatformBlock:
+        case Star:
+        case WaterEnemy:
+      }
+    }
+  }
+
+  void initializeGame() {
+    //Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
+
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+
+    world.add(_ember);
+  }
 
   @override
   Future<void> onLoad() async {
@@ -18,11 +50,6 @@ class EmberQuestGame extends FlameGame {
     ]);
 
     camera.viewfinder.anchor = Anchor.topLeft;
-
-    _ember = EmberPlayer(
-      position: Vector2(128, canvasSize.y - 70),
-    );
-
-    world.add(_ember);
+    initializeGame();
   }
 }
